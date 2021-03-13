ff7a73d84c3b216b000b9a36723ababca6946661
Timothy M Herron <tmherron09@gmail.com>
2021-03-12 18:34:45 -0600

    First Files

---
+++ Learning C++/Early Lessons/HelloWorld.cpp
@@ -0,0 +1,11 @@
+//Hello World
+// Learn Dev-C++
+#include <iostream>
+using namespace std;
+int main()
+{
+	string name;
+	cout << "Please Enter your name: ";
+	cin >> name;
+	cout << "Hello World! Hi, " << name << endl;
+}
